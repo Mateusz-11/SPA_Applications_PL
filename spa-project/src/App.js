b138b73910f675ea5ea5fe893ceb04c9fb1ac4b6
Mateusz-11 <tweak.night@gmail.com>
2023-03-29 19:23:51 +0200

    Try changes

--- spa-project/src/App.js
+++ spa-project/src/App.js
@@ -8,6 +8,10 @@ function App() {
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
+				</p>{" "}
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
 					className='App-link'
